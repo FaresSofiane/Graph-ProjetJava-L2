24608d21eb2bcd45f6996f0e1179e24768d164cb
Sofiane Fares <79220488+FaresSofiane@users.noreply.github.com>
2023-04-17 17:33:55 +0200

    0.1.0
    Graph Class created

---
+++ Graph.java
@@ -0,0 +1,14 @@
+public class Graph {
+
+    String name;
+
+    public Graph(String n){
+        this.name = n;
+    }
+    public String getName(){
+        return name;
+    }
+    public void ChangeName(String nameb){
+        this.name = nameb;
+    }
+}
